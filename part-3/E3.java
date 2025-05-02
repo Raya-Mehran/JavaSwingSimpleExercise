519bea5f6dcdd87f757f40ce73649f7858b9edb3
Raya-Mehran <sunshineatnoon1952@gmail.com>
2025-05-02 09:39:12 +0430

    part3 done

---
+++ part-3/E3.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class E3 extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JButton redButton;
+    private JButton greenButton;
+    private JButton blueButton;
+
+    public E3() {
+
+        mainPanel = new JPanel();
+
+        redButton = new JButton("Red");
+        greenButton = new JButton("Green");
+        blueButton = new JButton("Blue");
+
+        redButton.setActionCommand("RED");
+        greenButton.setActionCommand("GREEN");
+        blueButton.setActionCommand("BLUE");
+
+        redButton.addActionListener(this);
+        greenButton.addActionListener(this);
+        blueButton.addActionListener(this);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(redButton);
+        buttonPanel.add(greenButton);
+        buttonPanel.add(blueButton);
+
+        setLayout(new BorderLayout());
+        add(buttonPanel, BorderLayout.NORTH);
+        add(mainPanel, BorderLayout.CENTER);
+
+        this.setTitle("Theme Color Selector");
+        this.setSize(400, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "RED":
+                mainPanel.setBackground(Color.RED);
+                break;
+            case "GREEN":
+                mainPanel.setBackground(Color.GREEN);
+                break;
+            case "BLUE":
+                mainPanel.setBackground(Color.BLUE);
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        new E3();
+    }
+}
